2a0d38eb04f718f7dce4a261cec829de7a089781
yessinetrigui <yessinetrigui03@gmail.com>
2022-05-22 21:56:02 +0100

    add new condition to statique.php

--- BAC-2015/statique.php
+++ BAC-2015/statique.php
@@ -5,16 +5,21 @@ if(isset($_POST["G"])){
     $req = "select * from pilote where matricule='$sl';";
     $res = $conn->query($req);
     if($res->num_rows==0){
-        echo "pilote n'a acune vol !";
+        echo "pilote n'exist pas!";
     }else{
         $l = $res->fetch_array();
         echo "$l[1] $l[2] a effectué les vols suivants: ";
         $req = "select marque, sum(duree), count(datedep) from avion a, vol v where a.idavion = v.idavion AND matpilote='$sl' GROUP by v.idavion";
         $res = $conn->query($req);
-        while($l = $res->fetch_array()){
-            echo "Marque: $l[0] Total duree: $l[1]  nombre vols:$l[2]";
-
+        if($res->num_rows==0){
+            echo "pilote n'aucune vol!";
+        }else{
+            while($l = $res->fetch_array()){
+                echo "Marque: $l[0] Total duree: $l[1]  nombre vols:$l[2]";
+    
+            }
         }
+        
 
 
     }
